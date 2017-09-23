b359288c68eda0e89e77a32277a5189b9f23d6d1
hue9010 <hue9010@gmail.com>
2017-09-23 16:22:39 +0900

    init

---
+++ src/main/java/hue/CodesquadWhiteApplication.java
@@ -0,0 +1,12 @@
+package hue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesquadWhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesquadWhiteApplication.class, args);
+	}
+}
